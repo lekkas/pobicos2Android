1d3ea013daeadd56438d08846cccfd3fe0d21dd9
kalos <kalos@ubuntu.ubuntu-domain>
2011-12-03 11:31:10 +0200

    loadApp from file implementation

--- PoClient/src/org/lekkas/poclient/AppLoader/PoAppPillLoaderI.java
+++ PoClient/src/org/lekkas/poclient/AppLoader/PoAppPillLoaderI.java
@@ -3,5 +3,5 @@ package org.lekkas.poclient.AppLoader;
 public interface PoAppPillLoaderI {
 	public void loadAndStartApp();
 	public void killRunningApp();
-	//TODO: public void loadApp(byte[] app)
+	public void loadApp(String path);
 }
